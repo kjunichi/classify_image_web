871ccacf9a04ec566519633b708c1fe487dbc6ac
Junichi Kajiwara <junichi.kajiwara@gmail.com>
2017-04-16 20:30:55 +0900

    Support for safari and edge browser

--- build.py
+++ build.py
@@ -1,20 +1,26 @@
 import os
 import subprocess
+import shutil
 
 # ハッシュ 
 # https://github.com/tensorflow/models.git
 
 def getClassifyImage():
   subprocess.call(["git","clone","https://github.com/tensorflow/models.git"])
-  subprocess.call(["cd","models"])
+
+def patchClassifyImage():
+  os.chdir("models")
   subprocess.call(["git","checkout","7ff111ab514df86def76ee5140769b9edb51afd1"])
-  subprocess.call(["cd",".."])
-  # cp models/tutorials/image/imagenet/classify_image.py .
-  subprocess.call(["cp","models/tutorials/image/imagenet/classify_image.py","./classify_image_web.py"])
+  os.chdir("..")
+  src_path =  os.path.abspath("models/tutorials/image/imagenet/classify_image.py")
+  dest_path = os.path.abspath("./classify_image_web.py")
+  #print(src_path, dest_path)
+  shutil.copyfile(src_path, dest_path)
+  subprocess.call(["patch", "--binary","-u", "classify_image_web.py", "patch"])
 
 # ファイルの存在チェック
 if not os.path.exists("models"):
   print("found no classify_image.py!")
   getClassifyImage()
 
-subprocess.call(["patch", "-u", "classify_image_web.py", "patch"])
+patchClassifyImage()
